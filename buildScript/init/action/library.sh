cc93bff7e88cbe5cc5d9c7a0806e7c93891e21b8
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2021-11-26 23:15:13 +0800

    Update build scripts

--- buildScript/init/action/library.sh
+++ buildScript/init/action/library.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-git submodule update --init 'library/core/*'
-git submodule update --init 'external/*'
+rm -rf external
+mkdir -p external
+cd external
+
+# Download "external" from Internet
+
+wget -q -O tmp.zip https://github.com/SagerNet/editorkit/archive/e7d1d0dca2c3e9b313f28dc34df1e12282f89206.zip
+unzip tmp.zip > /dev/null 2>&1
+mv editorkit-* editorkit
+
+wget -q -O tmp.zip https://github.com/SagerNet/preferencex-android/archive/2bdf5a06bc242f5d5f01aa66b88ea640c938f243.zip
+unzip tmp.zip > /dev/null 2>&1
+mv preferencex-android-* preferencex
+
+wget -q -O tmp.zip https://github.com/SagerNet/termux-view/archive/c4127827ef013970bcd0f930f65b991bb12878f4.zip
+unzip tmp.zip > /dev/null 2>&1
+mv termux-view-* termux-view
+
+rm tmp.zip
--- buildScript/init/action/shadowsocks.sh
+++ buildScript/init/action/shadowsocks.sh
@@ -3,7 +3,8 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.#rustup.rs | sh -s -- --default-toolchain none -y
 echo "source \$HOME/.cargo/env" >>$HOME/.bashrc
 
-git submodule update --init library/shadowsocks/src/main/rust/shadowsocks-rust
+bash "buildScript/zipVersion/downloadZip.sh" library/shadowsocks/src/main/rust buildScript/zipVersion/shadowsocks_name buildScript/zipVersion/shadowsocks_status
+
 cd library/shadowsocks/src/main/rust/shadowsocks-rust
 rustup target install armv7-linux-androideabi aarch64-linux-android i686-linux-android x86_64-linux-android
 
