c8ec6f51f14dd6edf9f5ae239255c009c4b7b10d
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2022-04-02 22:37:59 +0800

    bump version

--- libcore/date.go
+++ libcore/date.go
@@ -7,7 +7,7 @@ import (
 var outdated string
 
 func GetBuildTime() int64 {
-	buildDate := "20220321"
+	buildDate := "20220402"
 	buildTime, _ := time.Parse("20060102", buildDate)
 	return buildTime.Unix()
 }
