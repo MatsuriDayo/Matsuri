7532fd324a8d8553e7825f283d6dd16aad9c6c62
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2022-05-29 21:32:53 +0800

    update & imporve fab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,15 @@ dependencies {
     compileOnly(project(":library:stub"))
     implementation(project(":library:include"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.activity:activity-ktx:1.4.0")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.browser:browser:1.4.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.work:work-runtime-ktx:2.7.1")
@@ -50,7 +50,7 @@ dependencies {
     implementation(project(":external:preferencex:preferencex"))
     implementation(project(":external:preferencex:preferencex-simplemenu"))
 
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.6.0")
     implementation("com.google.code.gson:gson:2.8.9")
 
     implementation("com.github.jenly1314:zxing-lite:2.1.1")
@@ -72,9 +72,9 @@ dependencies {
         exclude(group = "com.google.guava", module = "guava")
     }
 
-    implementation("androidx.room:room-runtime:2.4.1")
-    kapt("androidx.room:room-compiler:2.4.1")
-    implementation("androidx.room:room-ktx:2.4.1")
+    implementation("androidx.room:room-runtime:2.4.2")
+    kapt("androidx.room:room-compiler:2.4.2")
+    implementation("androidx.room:room-ktx:2.4.2")
     implementation("com.github.MatrixDev.Roomigrant:RoomigrantLib:0.3.4")
     kapt("com.github.MatrixDev.Roomigrant:RoomigrantCompiler:0.3.4")
 
--- app/src/main/java/io/nekohasekai/sagernet/ktx/Layouts.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ktx/Layouts.kt
@@ -19,11 +19,9 @@
 
 package io.nekohasekai.sagernet.ktx
 
-import android.graphics.Rect
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import io.nekohasekai.sagernet.ui.MainActivity
 
 class FixedLinearLayoutManager(val recyclerView: RecyclerView) :
     LinearLayoutManager(recyclerView.context, RecyclerView.VERTICAL, false) {
@@ -33,53 +31,6 @@ class FixedLinearLayoutManager(val recyclerView: RecyclerView) :
             super.onLayoutChildren(recycler, state)
         } catch (ignored: IndexOutOfBoundsException) {
         }
-    }
-
-    private var listenerDisabled = false
-
-    override fun scrollVerticallyBy(
-        dx: Int, recycler: RecyclerView.Recycler,
-        state: RecyclerView.State
-    ): Int {
-        val scrollRange = super.scrollVerticallyBy(dx, recycler, state)
-        if (listenerDisabled) return scrollRange
-        val activity = recyclerView.context as? MainActivity
-        if (activity == null) {
-            listenerDisabled = true
-            return scrollRange
-        }
-
-        val overscroll = dx - scrollRange
-        if (overscroll > 0) {
-            val view =
-                (recyclerView.findViewHolderForAdapterPosition(findLastVisibleItemPosition())
-                    ?: return scrollRange).itemView
-            val itemLocation = Rect().also { view.getGlobalVisibleRect(it) }
-            val fabLocation = Rect().also { activity.binding.fab.getGlobalVisibleRect(it) }
-            if (!itemLocation.contains(fabLocation.left, fabLocation.top) && !itemLocation.contains(fabLocation.right, fabLocation.bottom)) {
-                return scrollRange
-            }
-            activity.binding.fab.apply {
-                if (isShown) hide()
-            }
-        } else {
-            /*val screen = Rect().also { activity.window.decorView.getGlobalVisibleRect(it) }
-            val location = Rect().also { activity.stats.getGlobalVisibleRect(it) }
-            if (screen.bottom < location.bottom) {
-                return scrollRange
-            }
-            val height = location.bottom - location.top
-            val mH = activity.stats.measuredHeight
-
-            if (mH > height) {
-                return scrollRange
-            }*/
-
-            activity.binding.fab.apply {
-                if (!isShown) show()
-            }
-        }
-        return scrollRange
     }
 
 }
@@ -92,53 +43,6 @@ class FixedGridLayoutManager(val recyclerView: RecyclerView, spanCount: Int) :
             super.onLayoutChildren(recycler, state)
         } catch (ignored: IndexOutOfBoundsException) {
         }
-    }
-
-    private var listenerDisabled = false
-
-    override fun scrollVerticallyBy(
-        dx: Int, recycler: RecyclerView.Recycler,
-        state: RecyclerView.State
-    ): Int {
-        val scrollRange = super.scrollVerticallyBy(dx, recycler, state)
-        if (listenerDisabled) return scrollRange
-        val activity = recyclerView.context as? MainActivity
-        if (activity == null) {
-            listenerDisabled = true
-            return scrollRange
-        }
-
-        val overscroll = dx - scrollRange
-        if (overscroll > 0) {
-            val view =
-                (recyclerView.findViewHolderForAdapterPosition(findLastVisibleItemPosition())
-                    ?: return scrollRange).itemView
-            val itemLocation = Rect().also { view.getGlobalVisibleRect(it) }
-            val fabLocation = Rect().also { activity.binding.fab.getGlobalVisibleRect(it) }
-            if (!itemLocation.contains(fabLocation.left, fabLocation.top) && !itemLocation.contains(fabLocation.right, fabLocation.bottom)) {
-                return scrollRange
-            }
-            activity.binding.fab.apply {
-                if (isShown) hide()
-            }
-        } else {
-            /*val screen = Rect().also { activity.window.decorView.getGlobalVisibleRect(it) }
-            val location = Rect().also { activity.stats.getGlobalVisibleRect(it) }
-            if (screen.bottom < location.bottom) {
-                return scrollRange
-            }
-            val height = location.bottom - location.top
-            val mH = activity.stats.measuredHeight
-
-            if (mH > height) {
-                return scrollRange
-            }*/
-
-            activity.binding.fab.apply {
-                if (!isShown) show()
-            }
-        }
-        return scrollRange
     }
 
 }
