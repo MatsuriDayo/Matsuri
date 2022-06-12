c7f591b9f353e30100342df0f2083073f111b3bb
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2022-06-12 12:04:50 +0800

    try to build libcore for darwin

--- libcore/tun/tuns/tuns_other.go
+++ libcore/tun/tuns/tuns_other.go
@@ -1,3 +1,5 @@
+//go:build !linux
+
 package tuns
 
 import (
@@ -9,13 +11,13 @@ import (
 )
 
 func NewGvisor(dev int32, mtu int32, handler tun.Handler, nicId tcpip.NICID, pcap bool, pcapFile *os.File, snapLen uint32, ipv6Mode int32) (tun.Tun, error) {
-	return nil, errors.New("platform depended")
+	return nil, errors.New("not for your platform")
 }
 
 func NewSystem(dev int32, mtu int32, handler tun.Handler, ipv6Mode int32, errorHandler func(err string)) (tun.Tun, error) {
-	return nil, errors.New("platform depended")
+	return nil, errors.New("not for your platform")
 }
 
 func NewTun2Socket(fd int32, handler tun.Handler) (tun.Tun, error) {
-	return nil, errors.New("platform depended")
+	return nil, errors.New("not for your platform")
 }
