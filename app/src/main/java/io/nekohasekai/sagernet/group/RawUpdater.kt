64abb495ff7576a1a34c1f2bc54015e6fd7be9fd
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2022-10-13 17:01:03 +0800

    fix: clash vmess sni

--- app/src/main/java/io/nekohasekai/sagernet/group/RawUpdater.kt
+++ app/src/main/java/io/nekohasekai/sagernet/group/RawUpdater.kt
@@ -378,6 +378,9 @@ object RawUpdater : GroupUpdater() {
                                     }
                                 }
                             }
+                            if (bean.isTLS() && bean.sni.isNullOrBlank() && bean.host.isNotBlank()) {
+                                bean.sni = bean.host
+                            }
                             proxies.add(bean)
                         }
                         "trojan" -> {
