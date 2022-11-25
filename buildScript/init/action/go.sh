d0bb203d5af11275d8066eb5d52aae2b47a1063a
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2022-11-25 22:32:37 +0900

    update

--- buildScript/init/action/go.sh
+++ buildScript/init/action/go.sh
@@ -6,8 +6,8 @@ mkdir -p $GOPATH
 cd $golang
 
 if [ ! -f "go/bin/go" ]; then
-    curl -Lso go.tar.gz https://go.dev/dl/go1.18.6.linux-amd64.tar.gz
-    echo "bb05f179a773fed60c6a454a24141aaa7e71edfd0f2d465ad610a3b8f1dc7fe8 go.tar.gz" | sha256sum -c -
+    curl -Lso go.tar.gz https://go.dev/dl/go1.18.8.linux-amd64.tar.gz
+    echo "4d854c7bad52d53470cf32f1b287a5c0c441dc6b98306dea27358e099698142a go.tar.gz" | sha256sum -c -
     tar xzf go.tar.gz
 fi
 
