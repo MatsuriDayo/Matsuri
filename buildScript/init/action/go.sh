ac03e372406033cb92e444be69241f33b1b42eee
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-04-10 10:19:23 +0900

    fix: update script

--- buildScript/init/action/go.sh
+++ buildScript/init/action/go.sh
@@ -6,8 +6,8 @@ mkdir -p $GOPATH
 cd $golang
 
 if [ ! -f "go/bin/go" ]; then
-    curl -Lso go.tar.gz https://go.dev/dl/go1.20.linux-amd64.tar.gz
-    echo "5a9ebcc65c1cce56e0d2dc616aff4c4cedcfbda8cc6f0288cc08cda3b18dcbf1 go.tar.gz" | sha256sum -c -
+    curl -Lso go.tar.gz https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
+    echo "979694c2c25c735755bf26f4f45e19e64e4811d661dd07b8c010f7a8e18adfca go.tar.gz" | sha256sum -c -
     tar xzf go.tar.gz
 fi
 
--- buildScript/lib/assets.sh
+++ buildScript/lib/assets.sh
@@ -17,7 +17,7 @@ get_latest_release() {
 VERSION_V2RAY=`get_latest_release "v2fly/v2ray-core"`
 echo VERSION_V2RAY=$VERSION_V2RAY
 echo -n $VERSION_V2RAY > core.version.txt
-curl -LSsO https://github.com/v2fly/v2ray-core/releases/download/$VERSION_V2RAY/v2ray-extra.zip
+curl -fLSsO https://github.com/v2fly/v2ray-core/releases/download/$VERSION_V2RAY/v2ray-extra.zip
 unzip v2ray-extra.zip
 mv browserforwarder/* .
 xz index.js
@@ -26,14 +26,14 @@ xz index.js
 VERSION_GEOIP=`get_latest_release "v2fly/geoip"`
 echo VERSION_GEOIP=$VERSION_GEOIP
 echo -n $VERSION_GEOIP > geoip.version.txt
-curl -LSsO https://github.com/v2fly/geoip/releases/download/$VERSION_GEOIP/geoip.dat
+curl -fLSsO https://github.com/v2fly/geoip/releases/download/$VERSION_GEOIP/geoip.dat
 xz geoip.dat
 
 ####
 VERSION_GEOSITE=`get_latest_release "v2fly/domain-list-community"`
 echo VERSION_GEOSITE=$VERSION_GEOSITE
 echo -n $VERSION_GEOSITE > geosite.version.txt
-curl -LSs -o geosite.dat.xz https://github.com/v2fly/domain-list-community/releases/download/$VERSION_GEOSITE/dlc.dat.xz
+curl -fLSs -o geosite.dat.xz https://github.com/v2fly/domain-list-community/releases/download/$VERSION_GEOSITE/dlc.dat.xz
 
 ####
 rm -rf browserforwarder *.zip
